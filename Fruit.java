1ea5e6d6a3713b420c45d60b17fa1b5f038de3fd
beespoke-ltd <dangelodeniro95@gmail.com>
2025-06-22 15:25:35 -0600

    Added a couple things to the Fruit class.

--- Fruit.java
+++ Fruit.java
@@ -1,19 +1,31 @@
+
+/**
+* This is an abstract class for fruit to be inherit by specific fruits
+* @author         Susan S. Bai, D'Angelo L. DeNiro
+*/
 public class Fruit {
 
     protected String name;
-    protected final boolean hasSeeds = true;
+    private final boolean hasSeeds = true;
 
     String color;
     float weight;
-    float price;
-    
+    int salesPrice;
+    int costPrice;
 
     // Constructor
-    protected Fruit(String name, String color, float weight, float price) {
+    protected Fruit(
+        String name, 
+        String color, 
+        float weight, 
+        int salesPrice, 
+        int costPrice
+        ) {
         this.name = name;
         this.color = color;
         this.weight = weight;
-        this.price = price;
+        this.salesPrice = salesPrice;
+        this.costPrice = costPrice;
     }
     
 }
