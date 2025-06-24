7b17fac870ac057dcad19613c1460e75e2590f70
beespoke-ltd <dangelodeniro95@gmail.com>
2025-06-23 22:09:43 -0600

    Added

---
+++ Apple.java
@@ -0,0 +1,8 @@
+public class Apple extends Fruit {
+    
+    public Apple(String name, String color, int weightInPounds, int costPerPound) {
+        super(name, color, weightInPounds, costPerPound);
+    }
+
+    boolean isSour = false;
+}
---
+++ Lemon.java
@@ -0,0 +1,8 @@
+class Lemon extends Fruit {
+    
+    public Lemon(String name, String color, int weightInPounds, int costPerPound, boolean isSour) {
+        super(name, color, weightInPounds, costPerPound);
+    }
+
+    boolean isSour = true;
+}
