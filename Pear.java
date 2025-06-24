12c5861e63fd9055a1f53bf4ab198f81722625f2
beespoke-ltd <dangelodeniro95@gmail.com>
2025-06-23 22:17:22 -0600

    Pear

---
+++ Pear.java
@@ -0,0 +1,8 @@
+class Pear extends Fruit {
+
+    public Pear(String name, String color, int weightInPounds, int costPerPound) {
+        super(name, color, weightInPounds, costPerPound);
+    }
+
+    boolean isSour = false;
+}
